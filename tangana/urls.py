1e83278e5998e497df55d0715aa6f4e412067d99
guillemp <guillemzz@gmail.com>
2015-10-21 20:29:38 +0200

    Adding auth system

--- tangana/urls.py
+++ tangana/urls.py
@@ -4,8 +4,18 @@ from webapp import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    #main
+    # main
     url(r'^$', views.index, name='index'),
+    
+    # auth system
+    #url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}, name='login'),
+    url(r'^login/reset/$', 'django.contrib.auth.views.password_reset', {'template_name': 'reset.html', 'post_reset_redirect': '/login/reset/done/'}, name="password_reset"),
+    url(r'^login/reset/done/$', 'django.contrib.auth.views.password_reset_done', {'template_name': 'reset_done.html'}),
+    url(r'^login/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', {'template_name': 'reset_confirm.html', 'post_reset_redirect': '/login/reset/complete/'}),
+    url(r'^login/reset/complete/$', 'django.contrib.auth.views.password_reset_complete', {'template_name': 'reset_complete.html'}),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    
     #comments
     #url(r'^comments/$', views.comment_list, name='comment_list'),
     #url(r'^comment/new/$', views.comment_create, name='comment_create'),
